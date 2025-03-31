0594f458e86b39a6ef0c49e2f3fe9cb32f066038
unknown <tazza.personal@gmail.com>
2025-03-31 16:14:09 -0300

    front not used

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,6 @@
 import About from "../About/About"
 import Calderas from "../Calderas/Calderas"
 import ChooseUs from "../Choose Us/ChooseUs"
-import Contacto from "../Contacto/Contacto"
 import Inicio from "../Inicio/Inicio"
 import Services from "../Services/Services"
 import Trabajos from "../Trabajos/Trabajos"
